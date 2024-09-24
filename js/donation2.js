55f2b9a937a3a715ad37c013ec28b83f6463c92a
RIRony9898 <rirony59@gmail.com>
2024-09-24 16:57:00 +0600

    apply utility function

--- js/donation2.js
+++ js/donation2.js
@@ -1,112 +1,120 @@
-// donate noakhali function
-let donateNoakhali = document.getElementById("btn-donation-noakhali");
+// // donate noakhali function
+// let donateNoakhali = document.getElementById("btn-donation-noakhali");
 
-donateNoakhali.addEventListener("click", function () {
-  let donateNoakhali = document.getElementById(
-    "input-donation-amount-for-noakhali"
-  ).value;
+// donateNoakhali.addEventListener("click", function () {
+//   let donateNoakhali = document.getElementById(
+//     "input-donation-amount-for-noakhali"
+//   ).value;
 
-  if(isNaN(donateNoakhali) || donateNoakhali<=0) {
-    alert("Invalid Input!");
-    return;
-  }
+//   if(isNaN(donateNoakhali) || donateNoakhali<=0) {
+//     alert("Invalid Input!");
+//     return;
+//   }
 
-  let noakhaliBalance = parseFloat(document.getElementById("noakhaliBalance").innerText);
+//   let noakhaliBalance = parseFloat(document.getElementById("noakhaliBalance").innerText);
 
-  let newNoakhaliBalance = noakhaliBalance + parseFloat(donateNoakhali);
-
-
-  let balance = parseFloat(document.getElementById("balance").innerText);
-
-  let newBalance = balance - donateNoakhali;
-
-  if(newBalance < 0){
-    alert("Insufficient balance!");
-    return;
-  }else{
-    document.getElementById("balance").innerText = newBalance;
-    document.getElementById("noakhaliBalance").innerText = newNoakhaliBalance;
-    document.getElementById('my_modal_1').showModal();
-    document.getElementById("input-donation-amount-for-noakhali").value = '';
-  }
-
-  let cardTitleNoakhali = document.getElementById('card-title-noakhali').innerText;
-
-appendFunction(donateNoakhali, cardTitleNoakhali);
-});
+//   let newNoakhaliBalance = noakhaliBalance + parseFloat(donateNoakhali);
 
 
-// donate feni function
-let donateFeni = document.getElementById("btn-donation-feni");
+//   let balance = parseFloat(document.getElementById("balance").innerText);
 
-donateFeni.addEventListener("click", function () {
-  let donateFeni = document.getElementById(
-    "input-donation-amount-for-feni"
-  ).value;
+//   let newBalance = balance - donateNoakhali;
 
-  if(isNaN(donateFeni) || donateFeni <= 0) {
-    alert("Invalid Input!");
-    return;
-  }
+//   if(newBalance < 0){
+//     alert("Insufficient balance!");
+//     return;
+//   }else{
+//     document.getElementById("balance").innerText = newBalance;
+//     document.getElementById("noakhaliBalance").innerText = newNoakhaliBalance;
+//     document.getElementById('my_modal_1').showModal();
+//     document.getElementById("input-donation-amount-for-noakhali").value = '';
+//   }
 
-  let feniBalance = parseFloat(document.getElementById("feniBalance").innerText);
+//   let cardTitleNoakhali = document.getElementById('card-title-noakhali').innerText;
 
-  let newFeniBalance = feniBalance + parseFloat(donateFeni);
+// appendFunction(donateNoakhali, cardTitleNoakhali);
+// });
+
+
+// // donate feni function
+// let donateFeni = document.getElementById("btn-donation-feni");
+
+// donateFeni.addEventListener("click", function () {
+//   let donateFeni = document.getElementById(
+//     "input-donation-amount-for-feni"
+//   ).value;
+
+//   if(isNaN(donateFeni) || donateFeni <= 0) {
+//     alert("Invalid Input!");
+//     return;
+//   }
+
+//   let feniBalance = parseFloat(document.getElementById("feniBalance").innerText);
+
+//   let newFeniBalance = feniBalance + parseFloat(donateFeni);
 
   
-  let balance = parseFloat(document.getElementById("balance").innerText);
+//   let balance = parseFloat(document.getElementById("balance").innerText);
 
-  let newBalance = balance - donateFeni;
+//   let newBalance = balance - donateFeni;
   
-  if(newBalance < 0){
-    alert("Insufficient balance!");
-    return;
-  }else{
-    document.getElementById("balance").innerText = newBalance;
-    document.getElementById("feniBalance").innerText = newFeniBalance;
-    document.getElementById('my_modal_1').showModal();
-    document.getElementById("input-donation-amount-for-feni").value = '';
-  }
+//   if(newBalance < 0){
+//     alert("Insufficient balance!");
+//     return;
+//   }else{
+//     document.getElementById("balance").innerText = newBalance;
+//     document.getElementById("feniBalance").innerText = newFeniBalance;
+//     document.getElementById('my_modal_1').showModal();
+//     document.getElementById("input-donation-amount-for-feni").value = '';
+//   }
 
-  let cardTitleFeni = document.getElementById('card-title-feni').innerText;
+//   let cardTitleFeni = document.getElementById('card-title-feni').innerText;
 
-  appendFunction(donateFeni, cardTitleFeni);
-});
+//   appendFunction(donateFeni, cardTitleFeni);
+// });
 
 
-// donate quota injured function
-let donationQuotaInjured = document.getElementById("btn-donation-quota-injured");
+// // donate quota injured function
+// let donationQuotaInjured = document.getElementById("btn-donation-quota-injured");
 
-donationQuotaInjured.addEventListener("click", function () {
-  let donateQuotaInjured = document.getElementById(
-    "input-donation-amount-for-quota-injured"
-  ).value;
+// donationQuotaInjured.addEventListener("click", function () {
+//   let donateQuotaInjured = document.getElementById(
+//     "input-donation-amount-for-quota-injured"
+//   ).value;
 
-  if(isNaN(donateQuotaInjured) || donateQuotaInjured <= 0) {
-    alert("Invalid Input!");
-    return;
-  }
+//   if(isNaN(donateQuotaInjured) || donateQuotaInjured <= 0) {
+//     alert("Invalid Input!");
+//     return;
+//   }
 
-  let quotaInjuredBalance = parseFloat(document.getElementById("quotaInjuredBalance").innerText);
+//   // validateData(donateQuotaInjured);
 
-  let newQuotaInjuredBalance = quotaInjuredBalance + parseFloat(donateQuotaInjured);
+//   let quotaInjuredBalance = parseFloat(document.getElementById("quotaInjuredBalance").innerText);
 
-  let balance = parseFloat(document.getElementById("balance").innerText);
+//   let newQuotaInjuredBalance = quotaInjuredBalance + parseFloat(donateQuotaInjured);
 
-  let newBalance = balance - donateQuotaInjured;
+//   let balance = parseFloat(document.getElementById("balance").innerText);
+
+//   let newBalance = balance - donateQuotaInjured;
   
-  if(newBalance < 0){
-    alert("Insufficient balance!");
-    return;
-  }else{
-    document.getElementById("balance").innerText = newBalance;
-    document.getElementById("quotaInjuredBalance").innerText = newQuotaInjuredBalance;
-    document.getElementById('my_modal_1').showModal();
-    document.getElementById("input-donation-amount-for-quota-injured").value = '';
-  }
+//   if(newBalance < 0){
+//     alert("Insufficient balance!");
+//     return;
+//   }else{
+//     document.getElementById("balance").innerText = newBalance;
+//     document.getElementById("quotaInjuredBalance").innerText = newQuotaInjuredBalance;
+//     document.getElementById('my_modal_1').showModal();
+//     document.getElementById("input-donation-amount-for-quota-injured").value = '';
+//   }
 
-  let cardTitleQuotaInjured = document.getElementById('card-title-quota-injured').innerText;
+//   let cardTitleQuotaInjured = document.getElementById('card-title-quota-injured').innerText;
 
-  appendFunction(donateQuotaInjured, cardTitleQuotaInjured);
-});
+//   appendFunction(donateQuotaInjured, cardTitleQuotaInjured);
+// });
 
+
+
+// Call the function for each donation type
+handleDonation("btn-donation-noakhali", "input-donation-amount-for-noakhali", "noakhaliBalance", "card-title-noakhali");
+handleDonation("btn-donation-feni", "input-donation-amount-for-feni", "feniBalance", "card-title-feni");
+handleDonation("btn-donation-quota-injured", "input-donation-amount-for-quota-injured", "quotaInjuredBalance", "card-title-quota-injured");
--- js/utility.js
+++ js/utility.js
@@ -28,8 +28,39 @@ let appendFunction = function (trigger, reason) {
   sectionHistory.appendChild(createHistory);
 }
 
-
 // btn-blog
 document.getElementById('btn-blog').addEventListener('click', function(){
   window.location.href = 'blog.html';
 })
+
+// donation utility function
+function handleDonation(btnId, inputId, balanceId, cardTitleId) {
+  let btn = document.getElementById(btnId);
+  btn.addEventListener("click", function () {
+    let donationAmount = document.getElementById(inputId).value;
+
+    if (isNaN(donationAmount) || donationAmount <= 0) {
+      alert("Invalid Input!");
+      return;
+    }
+
+    let currentBalance = parseFloat(document.getElementById(balanceId).innerText);
+    let newBalance = currentBalance + parseFloat(donationAmount);
+
+    let totalBalance = parseFloat(document.getElementById("balance").innerText);
+    let newTotalBalance = totalBalance - donationAmount;
+
+    if (newTotalBalance < 0) {
+      alert("Insufficient balance!");
+      return;
+    } else {
+      document.getElementById("balance").innerText = newTotalBalance;
+      document.getElementById(balanceId).innerText = newBalance;
+      document.getElementById('my_modal_1').showModal();
+      document.getElementById(inputId).value = '';
+    }
+
+    let cardTitle = document.getElementById(cardTitleId).innerText;
+    appendFunction(donationAmount, cardTitle);
+  });
+}
